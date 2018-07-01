1d6f3dc3e5660bf2e8d8264d501c3ccf8258fdbd
briecodes <hello@brie.work>
2018-07-01 19:22:57 -0400

    userEvents controller rendered.

---
+++ app/controllers/api/v1/user_events_controller.rb
@@ -0,0 +1,39 @@
+class Api::V1::UserEventsController < ApplicationController
+    before_action :find_user, only: [:update, :show]
+
+    def index
+        @users = User.all
+        render json: @users
+    end
+
+    def show
+        render json: @user
+    end
+
+    def create
+        user = User.new(user_params)
+        if user.save
+            render json: user, status: :accepted
+        else
+            render json: {errors: user.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    def update
+        @user.update(user_params)
+        if @note.save
+            render json: @user, status: :accepted
+        else
+            render json: {errors: @user.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    private
+        def user_params
+            params.permit(:username, :password, :name)
+        end
+
+        def find_user
+            @user = User.find(params[:id])
+        end
+end
