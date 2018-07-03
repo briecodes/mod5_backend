be7a2cd232d9e1feed908bdcd191bc47513daba9
briecodes <hello@brie.work>
2018-07-03 15:47:47 -0400

    seed file refactoring.

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,6 @@ user_events_list = [
     {user_id: 3, event_id: 1},
     {user_id: 4, event_id: 1},
 
-    {user_id: 1, event_id: 2},
     {user_id: 3, event_id: 2},
     {user_id: 4, event_id: 2},
 
