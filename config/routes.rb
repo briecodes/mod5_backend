97ccb1c2d89c4f0a693576a9c6be204d29485d33
briecodes <hello@brie.work>
2018-07-05 11:57:49 -0400

    update route added to events.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
       namespace :v1 do
         resources :users, only: [:index, :show, :create]
         resources :user_events, only: [:index, :show, :create, :delete, :destroy]
-        resources :events, only: [:index, :show, :create]
+        resources :events, only: [:index, :show, :create, :update]
         resources :song_entries, only: [:index, :show, :create, :update]
       end
     end
