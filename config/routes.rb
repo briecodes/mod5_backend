47311f2f41c061ec4460e8209973a81442e54df4
briecodes <hello@brie.work>
2018-07-05 17:05:08 -0400

    routing edited for user updates.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   Rails.application.routes.draw do
     namespace :api do
       namespace :v1 do
-        resources :users, only: [:index, :show, :create]
+        resources :users, only: [:index, :show, :create, :update]
         resources :user_events, only: [:index, :show, :create, :delete, :destroy]
         resources :events, only: [:index, :show, :create, :update, :delete, :destroy]
         resources :song_entries, only: [:index, :show, :create, :update, :delete, :destroy]
