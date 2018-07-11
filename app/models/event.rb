466f479123a1043f814708c2e87956ea45383797
briecodes <hello@brie.work>
2018-07-11 12:00:32 -0400

    added new column to event.

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,10 @@ class Event < ApplicationRecord
     has_many :song_entries
     has_many :users, through: :song_entries
     has_many :user_events
+
+    validates :user_id, presence: true
+    validates :title, presence: true
+    validates :title, uniqueness: true
+    validates :location, presence: true
+    validates :description, presence: true
 end
--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,5 +1,5 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :title, :location, :description, :key_code, :active
+  attributes :id, :user_id, :title, :location, :description, :key_code, :active, :public
 
   belongs_to :user
   has_many :song_entries
